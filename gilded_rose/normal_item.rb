ca096e51985ecd26e1df838356f120f2075cc8e7
Jen Heilemann <jheilema@nerdery.com>
2016-03-25 17:46:19 -0400

    taking out the sulfuras references from the base item class

--- gilded_rose/normal_item.rb
+++ gilded_rose/normal_item.rb
@@ -7,9 +7,7 @@ class GildedRose
     def update_quality
       if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > min_quality
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality -= 1
-          end
+          item.quality -= 1
         end
       else
         if item.quality < max_quality
@@ -28,16 +26,12 @@ class GildedRose
           end
         end
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in -= 1
-      end
+      item.sell_in -= 1
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > min_quality
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality -= 1
-              end
+              item.quality -= 1
             end
           else
             item.quality = item.quality - item.quality
